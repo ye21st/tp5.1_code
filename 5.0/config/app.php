ff44b01d4f5e6fe1cc56181aadbd85ebbb056b47
ye21st <ye21st@gmail.com>
2018-11-20 23:20:43 +0800

    分页文件提取出来，放到对应文件夹当中。

--- 5.0/config/app.php
+++ 5.0/config/app.php
@@ -116,4 +116,10 @@ return [
     // 异常处理handle类 留空使用 \think\exception\Handle
     'exception_handle'       => '',
 
+    //分页配置
+    'paginate'               => [
+        'type'      => 'app\comm\page\Page1',
+        'var_page'  => 'page',
+        'list_rows' => 15,
+    ],
 ];
